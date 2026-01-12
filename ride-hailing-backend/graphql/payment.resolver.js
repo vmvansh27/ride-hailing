8cdc729f1678bdf502265acb2705fe976220c26b
Vansh Madaan <vansh.madaan@ibm.com>
2026-01-12 15:34:20 +0530

    Merged with main

---
+++ ride-hailing-backend/graphql/payment.resolver.js
@@ -0,0 +1,53 @@
+import {
+    GraphQLObjectType,
+    GraphQLInt,
+    GraphQLString,
+    GraphQLFloat,
+} from "graphql";
+
+import { Payment } from "../rest/models/index.js";
+
+// Payment Type
+export const PaymentType = new GraphQLObjectType({
+    name: "Payment",
+    fields: () => ({
+        payment_id: { type: GraphQLInt },
+        ride_id: { type: GraphQLInt },
+        amount: { type: GraphQLFloat },
+        method: { type: GraphQLString },
+        status: { type: GraphQLString },
+    }),
+});
+
+// QUERY
+export const paymentQueries = {
+    payment: {
+        type: PaymentType,
+        args: {
+            payment_id: { type: GraphQLInt },
+        },
+        resolve: (_, args) => {
+            return Payment.findByPk(args.payment_id);
+        },
+    },
+};
+
+// MUTATION
+export const paymentMutations = {
+    createPayment: {
+        type: PaymentType,
+        args: {
+            ride_id: { type: GraphQLInt },
+            amount: { type: GraphQLFloat },
+            method: { type: GraphQLString },
+        },
+        resolve: (_, args) => {
+            return Payment.create({
+                ride_id: args.ride_id,
+                amount: args.amount,
+                method: args.method,
+                status: "completed",
+            });
+        },
+    },
+};
